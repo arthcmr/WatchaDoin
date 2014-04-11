440e02830b0408e9dc933a8019d5c6281f08d1d6
Arthur Camara <arthurcamara@gmail.com>
2014-04-11 02:24:55 +0200

    Set up badges

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,6 +2,33 @@ chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+console.log('Browser Action');
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+//set the badge text that appears next to the icon
+//type can be "play", "pause" or "stop"
+function setBadge(type) {
+	if(typeof type === 'undefined') return;
+	switch(type) {
+		case 'play':
+			chrome.browserAction.setBadgeBackgroundColor({color:"#85B200"});
+			chrome.browserAction.setBadgeText({text: '►'});
+			break;
+		case 'pause':
+			chrome.browserAction.setBadgeBackgroundColor({color:"#FF9326"});
+			chrome.browserAction.setBadgeText({text: '•'});
+			break;
+		case 'stop':
+		default:
+		chrome.browserAction.setBadgeText({text: ''});
+			break;
+	}
+}
+
+setBadge("pause");
+
+setTimeout(function() {
+	setBadge("play");
+}, 10000);
+setTimeout(function() {
+	setBadge("stop");
+}, 20000);
