0e63b786c5592836d32841e8d071cb6bce36a5df
Arthur Camara <arthurcamara@gmail.com>
2014-04-11 09:57:40 +0200

    Move elapsed time, clock and state to background script

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,31 +4,231 @@ chrome.runtime.onInstalled.addListener(function (details) {
 
 console.log('Browser Action');
 
+var $, clock;
+var state = {
+	status: "idle",
+	text: "",
+	start: undefined,
+	stop: undefined
+}
+
+var history = [];
+
+function setJQuery(jQuery) {
+	$ = jQuery;
+}
+
+function setState(prop, value) {
+	state[prop] = value;
+}
+
+function getState() {
+	return state;
+}
+
+function stop() {
+	setState("status", "idle");
+	setBadge('stop');
+	if(clock) clock.stop();
+
+	addToHistory(state.text, state.start, state.end);
+	state = {
+		status: "idle",
+		text: "",
+		start: undefined,
+		stop: undefined
+	}
+}
+
+function run(element) {
+	setState("status", "running");
+	elapsedTime(element);
+}
+
+function addToHistory(text, start, stop) {
+	history.push({
+		text: text,
+		start: start,
+		stop: stop
+	});
+}
+
 //set the badge text that appears next to the icon
 //type can be "play", "pause" or "stop"
-function setBadge(type) {
+function setBadge(type, value) {
 	if(typeof type === 'undefined') return;
 	switch(type) {
 		case 'play':
 			chrome.browserAction.setBadgeBackgroundColor({color:"#85B200"});
 			chrome.browserAction.setBadgeText({text: '►'});
 			break;
-		case 'pause':
-			chrome.browserAction.setBadgeBackgroundColor({color:"#FF9326"});
-			chrome.browserAction.setBadgeText({text: '•'});
-			break;
 		case 'stop':
 		default:
-		chrome.browserAction.setBadgeText({text: ''});
+			chrome.browserAction.setBadgeBackgroundColor({color:"#999999"});
+			chrome.browserAction.setBadgeText({text: ''});
 			break;
 	}
-}
+	if(typeof value !== 'undefined') {
+		chrome.browserAction.setBadgeText({text: value});
+	}
+};
 
-setBadge("pause");
+function elapsedTime(placeholder) {
+	if(clock) clock.stop();
+	var timeString, badgeString;
 
-setTimeout(function() {
-	setBadge("play");
-}, 10000);
-setTimeout(function() {
-	setBadge("stop");
-}, 20000);
+	clock = countUp({
+		start: state.start,
+		onTick: function(days, hours, minutes, seconds) {
+			days_s = ("0" + days).slice(-2);
+			hours_s = ("0" + hours).slice(-2);
+			minutes_s = ("0" + minutes).slice(-2);
+			seconds_s = ("0" + seconds).slice(-2);
+
+			if(days_s!="00") {
+				timeString = days_s+":"+hours_s+":"+minutes_s+":"+seconds_s;
+			}
+			else if(hours_s!="00") {
+				timeString = hours_s+":"+minutes_s+":"+seconds_s;
+			}
+			else {
+				timeString = minutes_s+":"+seconds_s;
+			}
+			if(days > 0) {
+				badgeString = days+"d";
+			}
+			else if(hours > 0) {
+				badgeString = hours+"h";
+			}
+			else if(minutes > 0) {
+				badgeString = minutes+"m";
+			}
+			else {
+				badgeString = seconds+"s";
+			}
+			placeholder.text(timeString);
+			setBadge('play', badgeString);
+		},
+		onStart: function(time) {
+			setState('start', time);
+			setState('stop', undefined);
+		},
+		onStop: function(time) {
+			setState('stop', time);
+		}
+	});
+	clock.run();
+
+};
+
+//countup
+function countUp (prop) {
+
+	prop = prop || {};
+
+	var days	= 24*60*60,
+		hours	= 60*60,
+		minutes	= 60
+		now = new Date();
+
+	var options = {};
+	options.start = prop.start || new Date();
+	options.onTick = prop.onTick || function(){};
+	options.onStart = prop.onStart || function(){};
+	options.onStop = prop.onStop || function(){};
+
+	var passed = 0, elapsed, d, h, m, s, clock;
+
+	//do this every second		
+	function tick(){
+		now = new Date();
+		passed = Math.floor((now - options.start) / 1000);
+		// Number of days passed
+		d = Math.floor(passed / days);
+		passed -= d*days;
+		// Number of hours
+		h = Math.floor(passed / hours);
+		passed -= h*hours;
+		// Number of minutes 
+		m = Math.floor(passed / minutes);
+		passed -= m*minutes;
+		// Number of seconds
+		s = passed;
+		// Calling an optional user supplied callback
+		options.onTick(d, h, m, s);
+	}
+	function run() {
+		options.onStart(options.start);
+		tick();
+		clock = setInterval(tick, 1000);
+	}
+	function stop() {
+		options.onStop(now);
+		clearInterval(clock);
+	}
+
+	return {
+		run: run,
+		stop: stop
+	}
+};
+
+// setBadge("pause");
+
+// setTimeout(function() {
+// 	setBadge("play");
+// }, 10000);
+// setTimeout(function() {
+// 	setBadge("stop");
+// }, 20000);
+
+
+// var storeList = Array();
+// var ambiente = 'www';
+// var instalacao = '000';
+
+// function S4() {
+//    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+// }
+// function guid() {
+//    return (instalacao+"-"+S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+// }
+
+// if (localStorage["localid"]){
+// 	var localid = localStorage["localid"];
+// }else{
+// 	localStorage["localid"] = guid();
+// 	var localid = localStorage["localid"];
+// }
+
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+//     if (changeInfo.status === 'complete') {
+//     	if (storeList.length > 0){
+// 			chrome.tabs.getSelected(null, function(tab) {
+// 			  chrome.tabs.sendMessage(tabId, {mensagem: "executar", sL: storeList, aB: ambiente, lId: localid}, function() {});
+// 			});    		
+//     	}else{
+//     		atualizaLista(tabId);
+//     	}
+// 	}
+// });
+
+// function atualizaLista(tabId){
+// 	var xhr = new XMLHttpRequest();
+// 	xhr.onreadystatechange = function(e){
+// 	    if (xhr.readyState == 4) {
+// 	    	if (xhr.responseText != ''){
+// 				storeList= JSON.parse(xhr.responseText);
+// 				if (tabId){
+// 					chrome.tabs.getSelected(null, function(tab) {
+// 					  chrome.tabs.sendMessage(tabId, {mensagem: "executar", sL: storeList, aB: ambiente, lId: localid}, function() {});
+// 					});    							
+// 				}	    		
+// 	    	}
+// 		}		
+// 	}; // Implemented elsewhere.
+// 	xhr.open("GET", 'http://'+ambiente+'.baixou.com.br/extensions/storeList/', true);
+// 	xhr.send(null);	
+// }
+
+// atualizaLista();
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,31 +1,48 @@
 //popup script
 
+var background = chrome.extension.getBackgroundPage();
+
 $(function() {
+
+	//TO DO: remove this workaround
+	background.setJQuery($);
 
 	var buttons_area = $("#buttons");
 	var text_field = $("#activity");
-	var state;
+	var state = background.getState();
 
-	start();
+	function init () {
 
-	//removeError class
-	text_field.bind('keyup', function(e) {
-		var text = text_field.val();
-		if(text != '') {
-			text_field.parent().removeClass("has-error");
+		text_field.val(state.text);
+		//removeError class
+		text_field.bind('keyup', function(e) {
+			var text = text_field.val();
+			background.setState('text', text);
+
+			if(text != '') {
+				text_field.parent().removeClass("has-error");
+			}
+
+			var code = e.keyCode || e.which;
+			if(code == 13) { //Enter keycode
+			   switchState();
+			}
+		});
+
+		if(state.status === "running") {
+			run();
 		}
-
-		var code = e.keyCode || e.which;
-		if(code == 13) { //Enter keycode
-		   switchState();
+		else {
+			start();
 		}
-	});
+	}
 
 	function start() {
 		//text should be empty
-		state = "start";
 		text_field.val('');
 		text_field.focus();
+
+		background.setBadge("");
 		//button should be start button
 		buttons_area.html('<button type="button" id="start" class="btn btn-primary"><span class="glyphicon glyphicon-play"></span> Start activity</button>');
 
@@ -46,48 +63,22 @@ $(function() {
 			return;
 		}
 
-		state = "running";
-
 		//buttons should be pause and stop buttons
 		buttons_area.html('<span class="btn btn-default" disabled><span class="glyphicon glyphicon-time"></span> <span id="elapsedTime"></span></span> <button type="button" id="done" class="btn btn-success"><span class="glyphicon glyphicon-ok"></span> Done</button>');
 
-		elapsedTime();
+		background.run($("#elapsedTime"));
+
 		var button_done = buttons_area.find("#done");
 		button_done.click(function() {
+			background.stop();
 			text_field.focus();
 			start();
 		});
 	}
 
-	function elapsedTime() {
-		var elapsedTimePlaceholder = $("#elapsedTime"),
-			timeString = "00:00";
-		elapsedTimePlaceholder.text(timeString);
-
-		var clock = countUp({
-			callback: function(days, hours, minutes, seconds) {
-				days = ("0" + days).slice(-2);
-				hours = ("0" + hours).slice(-2);
-				minutes = ("0" + minutes).slice(-2);
-				seconds = ("0" + seconds).slice(-2);
-
-				if(days!="00") {
-					timeString = days+":"+hours+":"+minutes+":"+seconds;
-				}
-				else if(hours!="00") {
-					timeString = hours+":"+minutes+":"+seconds;
-				}
-				else {
-					timeString = minutes+":"+seconds;
-				}
-				elapsedTimePlaceholder.text(timeString);
-			}
-		});
-
-	}
-
 	function switchState() {
-		if(state === "start") {
+		state = background.getState();
+		if(state.status === "idle") {
 			run();
 		}
 		else {
@@ -96,53 +87,6 @@ $(function() {
 		return false;
 	}
 
-	//countup
-	function countUp(prop) {
-
-		var days	= 24*60*60,
-			hours	= 60*60,
-			minutes	= 60;
-
-		var options = $.extend({
-				callback	: function(){},
-				start		: new Date()
-			},prop);
-
-		var passed = 0, elapsed, d, h, m, s, clock;
-
-		//do this every second		
-		function tick(){
-			var now = new Date();
-			passed = Math.floor((now - options.start) / 1000);
-			// Number of days passed
-			d = Math.floor(passed / days);
-			passed -= d*days;
-			// Number of hours
-			h = Math.floor(passed / hours);
-			passed -= h*hours;
-			// Number of minutes 
-			m = Math.floor(passed / minutes);
-			passed -= m*minutes;
-			// Number of seconds
-			s = passed;
-			// Calling an optional user supplied callback
-			if($.isFunction(options.callback)) {
-				options.callback(d, h, m, s);
-			}
-		}
-		function run() {
-			tick();
-			clock = setInterval(tick, 1000);
-		}
-		function pause() {
-			clearInterval(clock);
-		}
-		run();
-
-		return {
-			run: run,
-			pause: pause
-		}
-	}
+	init();
 
 });
