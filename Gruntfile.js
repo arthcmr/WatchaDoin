782fbfda7c029146a7e99dcdf77d55114277c08a
Arthur Camara <arthurcamara@gmail.com>
2014-04-11 02:22:19 +0200

    Remove version updating

--- Gruntfile.js
+++ Gruntfile.js
@@ -260,6 +260,19 @@ module.exports = function (grunt) {
 
         // Auto buildnumber, exclude debug files. smart builds that event pages
         chromeManifest: {
+            test: {
+                options: {
+                    buildnumber: false,
+                    background: {
+                        target: 'scripts/background.js',
+                        exclude: [
+                            'scripts/chromereload.js'
+                        ]
+                    }
+                },
+                src: '<%= config.app %>',
+                dest: '<%= config.dist %>'
+            },
             dist: {
                 options: {
                     buildnumber: true,
@@ -317,6 +330,20 @@ module.exports = function (grunt) {
         'usemin',
         'compress'
     ]);
+
+    grunt.registerTask('buildtest', [
+        'clean:dist',
+        'chromeManifest:test',
+        'useminPrepare',
+        'concurrent:dist',
+        'cssmin',
+        'concat',
+        'uglify',
+        'copy',
+        'usemin',
+        'compress'
+    ]);
+
 
     grunt.registerTask('default', [
         'jshint',
